18beb2f4a17ea0f2c9a3aa9915584ca008795dc7
Siwanad Singtong <siwanad.diamond@gmail.com>
2023-10-21 01:41:38 +0700

    first commit

--- src/state/api.js
+++ src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-  reducerPath: "addminApi",
+  reducerPath: "adminApi",
   tagTypes: ["User"],
   endpoints: (build) => ({
     getUser: build.query({
